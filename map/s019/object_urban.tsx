a48366df4cea3ec65d1681b94b6d5d997aab1f70
seophohoho <seophohoho@gmail.com>
2026-03-16 20:51:57 +0900

    update map data

---
+++ map/s019/object_urban.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="object_urban" tilewidth="16" tileheight="16" tilecount="3843" columns="63">
+ <image source="../../tiles/png/outdoor/object_urban.png" trans="fff568" width="1008" height="976"/>
+</tileset>

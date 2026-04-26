1ab901cb7601a4d9f19c54ebd4b77fb1cc548d3d
seophohoho <seophohoho@gmail.com>
2026-04-26 11:06:42 +0900

    update map data

---
+++ map/s003/object_urban.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="object_urban" tilewidth="16" tileheight="16" tilecount="3220" columns="46">
+ <image source="../../tiles/png/outdoor/object_urban.png" trans="e9a042" width="736" height="1120"/>
+</tileset>
---
+++ map/s003/urban.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="urban" tilewidth="16" tileheight="16" tilecount="3666" columns="39">
+ <image source="../../tiles/png/outdoor/urban.png" trans="e9a042" width="624" height="1504"/>
+</tileset>

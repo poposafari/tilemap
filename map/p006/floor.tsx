b5451e4b81286cb874dd3db066943e3b836c8a33
seophohoho <seophohoho@gmail.com>
2026-05-02 14:46:55 +0900

    update map data

---
+++ map/p006/floor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="floor" tilewidth="16" tileheight="16" tilecount="3024" columns="24">
+ <image source="../../tiles/png/indoor/floor.png" trans="e9a042" width="384" height="2016"/>
+</tileset>
---
+++ map/p006/object.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="object" tilewidth="16" tileheight="16" tilecount="11284" columns="62">
+ <image source="../../tiles/png/indoor/object.png" trans="e9a042" width="992" height="2912"/>
+</tileset>
